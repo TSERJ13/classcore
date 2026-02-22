1e71b2598e2ebf640cb5a7aed30d7d45d3d92ca2
Sergi Tsivtsivadze <tserj13@icloud.com>
2026-02-23 01:50:45 +0400

    Convert configs to ESM and trigger build

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
+
+export default config;
